858e71486033c97c2e0487f76f9498f73aacd7c1
Anton <d1Av0lo3124@gmail.com>
2018-04-12 20:00:50 +0200

    iteratos and etc

--- src/main/java/ueb03/StackImpl.java
+++ src/main/java/ueb03/StackImpl.java
@@ -2,37 +2,39 @@ package ueb03;
 
 import java.util.NoSuchElementException;
 
-public class StackImpl implements Stack {
-	class Element {
-		char value;
-		Element next;
-		Element(char value, Element next) {
-			this.value = value;
-			this.next = next;
+
+	public class StackImpl<T extends Comparable<T>> implements Stack<T> {
+		class Element {
+			T value;
+			Element next;
+
+			Element(T value, Element next) {
+				this.value = value;
+				this.next = next;
+			}
+		}
+
+		private Element top;
+		private int s = 0;
+
+		@Override
+		public void push(T c) {
+			top = new Element(c, top);
+			s++;
+		}
+
+		@Override
+		public T pop() {
+			if (top == null)
+				throw new NoSuchElementException();
+			T v = top.value;
+			top = top.next;
+			s--;
+			return v;
+		}
+
+		@Override
+		public int size() {
+			return s;
 		}
 	}
-
-	private Element top;
-	private int s = 0;
-
-	@Override
-	public void push(char c) {
-		top = new Element(c, top);
-		s++;
-	}
-
-	@Override
-	public char pop() {
-		if (top == null)
-			throw new NoSuchElementException();
-		char v = top.value;
-		top = top.next;
-		s--;
-		return v;
-	}
-
-	@Override
-	public int size() {
-		return s;
-	}
-}
